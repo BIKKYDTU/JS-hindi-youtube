27c70f0eff00de61329680012a94025e35af55f6
Bikky Kumar <bikkykumar_se22a11_32@dtu.ac.in>
2024-12-04 02:24:38 +0000

    datatypes

---
+++ .devcontainer/01_basic/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; //treate all js code as newer version
+
+// alert(3+3)
+
+console.log(3+3)
+
+console.log("bikky")
+
+let name = "bikky"
+let age = 18
+let isloghedIn = false
+
+// number => 2 to power 53
+// bigint 
+// string => ""
+// boolean => true/false
+// null => standlone value 
+// undefined
+// symbol => unique
+
+// object 
+
+console.log(typeof "bikky");
