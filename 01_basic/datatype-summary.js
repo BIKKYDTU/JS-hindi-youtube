774a6f8b57ef8eb3617f9a8ba62074ffddaa3391
Bikky Kumar <bikkykumar_se22a11_32@dtu.ac.in>
2024-12-04 22:37:46 +0000

    datatypesummary

---
+++ 01_basic/datatype-summary.js
@@ -0,0 +1,40 @@
+// primitive datatypes
+
+// 7 types: string, number, null,undefined,symbol,bigint
+
+const score = 100
+const scorevalue =100.3
+
+const isloggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = symbol('123')
+const anotherId = symbol('123')
+
+console.log(id === anotherId);
+
+
+
+
+
+// reference type(non primitive)
+
+// array , objects,functin
+
+const heros = ["shaktiman", "naagraj", "doga"];
+let myObj = {
+    name : "bikky",
+    age : 23,
+}
+
+const myfunction= function(){
+    console.log("hello world");
+    
+}
+
+console.log(typeof outsideTemp);
+
+
+
+
