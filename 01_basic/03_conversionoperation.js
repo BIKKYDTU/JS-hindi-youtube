a7da46e5d23a91e569e735174998647eb476a9f4
Bikky Kumar <bikkykumar_se22a11_32@dtu.ac.in>
2024-12-04 07:52:25 +0000

    conversionoperation

---
+++ 01_basic/03_conversionoperation.js
@@ -0,0 +1,28 @@
+let score = "undefined"
+
+console.log(typeof score);
+
+console.log(typeof(score));
+
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+// "33" => 33
+// "33jnghy" => NaN
+// true => 1; false => 0
+//
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+
+// 1 => true
+// " " => false
+// " hitesh"=> true
+
+let someNumber = 33
+
+let stringNumber = string(someNumber)
+console.log( stringNumber);
+
