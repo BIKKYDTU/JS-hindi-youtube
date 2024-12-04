8e4b3dab85cc55c6ff59642122f5540105b7e5a7
Bikky Kumar <bikkykumar_se22a11_32@dtu.ac.in>
2024-12-04 10:36:23 +0000

    conversion lec7

--- 01_basic/03_conversionoperation.js
+++ 01_basic/03_conversionoperation.js
@@ -1,28 +1,58 @@
-let score = "undefined"
+// let score = "undefined"
 
-console.log(typeof score);
+// console.log(typeof score);
 
-console.log(typeof(score));
+// console.log(typeof(score));
 
 
-let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
 // "33" => 33
 // "33jnghy" => NaN
 // true => 1; false => 0
 //
 
-let isLoggedIn = 1
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// let isLoggedIn = 1
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
 
 
 // 1 => true
 // " " => false
 // " hitesh"=> true
 
-let someNumber = 33
+// let someNumber = 33
 
-let stringNumber = string(someNumber)
-console.log( stringNumber);
+// let stringNumber = string(someNumber)
+// console.log( stringNumber);
+
+// ****************** operation ******************
+// let value = 3
+// let negValue = -value
+// console.log(negValue);
+// console.log(negValue);
+
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " bikky"
+
+// 
+console.log(true);
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2+2
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+
+
 
