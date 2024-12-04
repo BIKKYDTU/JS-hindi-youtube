d5079c14a822ef3fc2185fac6d5cf37db9a095e1
Bikky Kumar <bikkykumar_se22a11_32@dtu.ac.in>
2024-12-04 02:25:08 +0000

    variables

---
+++ .devcontainer/01_basic/01_variables.js
@@ -0,0 +1,12 @@
+const accountId =144556
+let accountEmail = "kumarbikky8340@gmail.com"
+var accountPassward = "12345"
+accountCity = "Jaipur"
+
+// accountId = 2 not allowed
+accountemail = "hc@hc.com"
+accountpassward ="267456"
+accountCity = "Bengaluru"
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassward,accountCity])
