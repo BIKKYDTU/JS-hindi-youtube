06c822455d1f1cb69701198b3f7ab5e54080ba33
Bikky Kumar <bikkykumar_se22a11_32@dtu.ac.in>
2024-12-04 23:03:45 +0000

    stack&heap

--- 01_basic/datatype-summary.js
+++ 01_basic/datatype-summary.js
@@ -2,17 +2,17 @@
 
 // 7 types: string, number, null,undefined,symbol,bigint
 
-const score = 100
-const scorevalue =100.3
+// const score = 100
+// const scorevalue =100.3
 
-const isloggedIn = false
-const outsideTemp = null
-let userEmail;
+// const isloggedIn = false
+// const outsideTemp = null
+// let userEmail;
 
-const id = symbol('123')
-const anotherId = symbol('123')
+// const id = symbol('123')
+// const anotherId = symbol('123')
 
-console.log(id === anotherId);
+// console.log(id === anotherId);
 
 
 
@@ -22,18 +22,41 @@ console.log(id === anotherId);
 
 // array , objects,functin
 
-const heros = ["shaktiman", "naagraj", "doga"];
-let myObj = {
-    name : "bikky",
-    age : 23,
-}
+// const heros = ["shaktiman", "naagraj", "doga"];
+// let myObj = {
+//     name : "bikky",
+//     age : 23,
+// }
 
-const myfunction= function(){
-    console.log("hello world");
+// const myfunction= function(){
+//     console.log("hello world");
     
-}
+// }
 
-console.log(typeof outsideTemp);
+// console.log(typeof outsideTemp);
+
+
+// ***********************   ********************
+// stack(premative), Heap(non premitive)
+
+let myYoutubename = "jycolyra.com "
+let anothername = myYoutubename
+anothername = "chaiurcode"
+  
+console.log(myYoutubename);
+console.log(anothername);
+
+let userOne ={
+    email: "user@google.com",
+    upi: "user@ybl"
+}
+  
+let userTwo = userOne
+
+userTwo.email = "amritesh@gmail.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
 
 
 
