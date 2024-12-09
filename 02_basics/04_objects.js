2688533bd2b22d771db8c187fa16b66299510b75
Bikky Kumar <bikkykumar_se22a11_32@dtu.ac.in>
2024-12-09 14:05:29 +0000

    jasean talk

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -1,36 +1,62 @@
 // const tinderUser = new Object() 
- const tinderUser = {}
+//  const tinderUser = {}
 
- tinderUser.id = "123abc"
-tinderUser.name = "bikky"
-tinderUser.isLoggedIn = false
+//  tinderUser.id = "123abc"
+// tinderUser.name = "bikky"
+// tinderUser.isLoggedIn = false
 // console.log(tinderUser);
-const regularUser = {
-    email: "bikky1610@gmail.com",
-    fullname : {
-        userfullname:{
-        firstname:"bikky",
-            lastname:"kumar"
-        }
-    }
-}
+// const regularUser = {
+//     email: "bikky1610@gmail.com",
+//     fullname : {
+//         userfullname:{
+//         firstname:"bikky",
+//             lastname:"kumar"
+//         }
+//     }
+// }
 
 // console.log(regularUser.fullname);
-const Obj1 ={1:"a", 2:"b"}
-const Obj2 ={3: "a", 4:"b"}
-// const Obj3 = {Obj1, Obj2}
-// const Obj3 = Object.assign({},Obj1,Obj2);
-const Obj3 = {...Obj1, ...Obj2}
-console.log(Obj3);
+// const Obj1 ={1:"a", 2:"b"}
+// const Obj2 ={3: "a", 4:"b"}
+// // const Obj3 = {Obj1, Obj2}
+// // const Obj3 = Object.assign({},Obj1,Obj2);
+// const Obj3 = {...Obj1, ...Obj2}
+// console.log(Obj3);
 
-const user =[
-    {
-        id:1,
-        email: "h@gmail.com"
-    },
-]
-user[1].email
-console.log(tinderUser);
-console.log(Object.keys(tinderUser));
-console.log(Object.values(tinderUser));
+// const user =[
+//     {
+//         id:1,
+//         email: "h@gmail.com"
+//     },
+// ]
+// user[1].email
+// console.log(tinderUser);
+// console.log(Object.keys(tinderUser));
+//console.log(Object.values(tinderUser));
 // YE CODE MR ERROR SHOW KAR RAHA HAI
+
+const course = {
+    coursename: "js in hindi",
+    price: "888",
+    courseInstructor: "hitesh"
+}
+
+//course.courseInstructor
+const {courseInstructor:instructor} = course
+
+//console.log(courseInstructor);
+console.log(instructor);
+
+// {
+//    "name": "bikky",
+//     "coursename" = "js in hindi",
+//     "price": " free"
+// }
+
+
+[
+    {},
+    {},
+    {},
+
+]
