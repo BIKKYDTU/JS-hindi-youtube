535bd2fe7de329b3eaf4b64ce69d491341f5f4c2
Bikky Kumar <bikkykumar_se22a11_32@dtu.ac.in>
2024-12-10 13:27:21 +0000

    one

---
+++ 04_controlflow/one.js
@@ -0,0 +1,61 @@
+// if
+//const isuserloggedIn = true
+//const temperature = 41
+
+// if(temperature< 50){
+//     console.log("less than 50");
+    
+// }
+// else{
+//     console.log("temperature is greater than 50");
+    
+// }
+// console.log("temperature is greater than 50");
+
+// if(2=="2"){
+//    console.log("executed");
+   
+// }
+
+// <, >, <=, >=, == , !=, ===, !==
+
+
+// const score = 200
+// if(score>100){
+//     const power = "fly"
+//     console.log(`user power: ${power}`);
+    
+// }
+// console.log(`user power: ${power}`);
+
+
+const blance = 1000
+//if(blance>500) console.log("test");
+
+// if(balance>500){
+//     console.log("less than");
+// } else if (balance<750){
+//    console.log("less than 750");
+   
+// }else if(blance <900){
+//     console.log("less than 750");
+    
+// }else{
+//     console.log("less than 1200");
+    
+// }
+
+
+const userloggedin = true
+const debitcard = true
+const loggedinfromgoogle = false
+const loggedinfromemail = true 
+
+if(userloggedin && debitcard){
+    console.log("Allow to buy course");
+    
+}
+if (loggedinfromgoogle || loggedinfromemail){
+    console.log("user logged in");
+    
+}
