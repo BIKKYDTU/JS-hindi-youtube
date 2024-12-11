214d79f1df717c4bb33ede5390e646466d70ad95
Bikky Kumar <bikkykumar_se22a11_32@dtu.ac.in>
2024-12-11 02:46:35 +0000

    switch2

---
+++ 04_controlflow/switch.js
@@ -0,0 +1,33 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+
+
+const month = "march"
+
+switch (month) {
+    case "jan":
+        console.log("january");
+        break;
+    case "feb":
+        console.log("january");
+        break;
+    case "feb":
+        console.log("feburary");
+        break;
+    case "march":
+        console.log("march");
+        break;
+
+    default:
+        console.log("default case match");
+        
+        break;
+}
+
