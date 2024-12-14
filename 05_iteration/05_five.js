c46b3daa8d6b0dd86a7ea76fab6b428c9340877e
Bikky Kumar <bikkykumar_se22a11_32@dtu.ac.in>
2024-12-14 05:36:57 +0000

    FIVE loop

---
+++ 05_iteration/05_five.js
@@ -0,0 +1,41 @@
+const coding =["js", "ruby","java", "python", "cpp"]
+
+// coding.forEach(function (val){
+//     console.log(val);
+    
+// } )
+
+// function printme(item){
+//     console.log(item);
+    
+// }
+
+// coding.forEach(printme)
+
+// coding.forEach((item,index , arr) => {
+//     console.log(item,index, arr);
+    
+// })
+
+
+const myCoding = [
+    {
+        languagename:"java",
+        languagefilename:"java"
+    },
+    {
+        languagename:"python",
+        languagefilename:"py"
+    },
+    {
+        languagename:"c++",
+        languagefilename:"cp"
+    },
+]
+
+myCoding.forEach((item) =>{
+   console.log(item.languagename);
+   
+})
+
+
